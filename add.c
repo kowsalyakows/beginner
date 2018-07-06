daa6591a3de1a3fdecfd65e872f7a7baaa97eb30
kowsalyakows <40886496+kowsalyakows@users.noreply.github.com>
2018-07-06 15:19:32 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
+#include <stdio.h>
+int main()
 {
   int n;
-  scanf("%d",&n)
+  scanf("%d",&n);
     if(n>0)
       printf("positive");
   else if(n<0)
     printf("negative");
   else
     printf("zero");
-  getch();
+
+    return 0;
 }
