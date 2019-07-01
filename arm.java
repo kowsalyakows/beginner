91e98939e0359f5045cfb9a4d793eeb847bd26b0
kowsalyakows <40886496+kowsalyakows@users.noreply.github.com>
2019-07-01 12:38:20 +0530

    Create arm.java

---
+++ arm.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n,a,d;
+        n=s.nextInt();
+        a=s.nextInt();
+        d=s.nextInt();
+        
+        
+        System.out.println(n*(a+d));
+    }
+}
